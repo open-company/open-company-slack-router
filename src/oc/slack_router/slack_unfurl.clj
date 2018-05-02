ddb92d85b42f4063fdef874efc5a9ba2d8fa3bc1
Nathan Zorn <nathan.zorn@gmail.com>
2018-05-02 16:03:38 -0400

    working org and section urls.

--- src/oc/slack_router/slack_unfurl.clj
+++ src/oc/slack_router/slack_unfurl.clj
@@ -31,29 +31,100 @@
   {:headers {"Content-Type" "application/vnd.open-company.entry.v1+json"
              "Authorization" (str "Bearer " token)}})
 
+(defn storage-request-org-url
+  [org]
+  ;; /orgs/:org-slug
+  (str config/storage-server-url
+       "/orgs/"
+       org))
+
+(defn storage-request-section-url
+  [org section]
+  ;; /orgs/:org-slug/boards/:slug
+  (str config/storage-server-url
+       "/orgs/"
+       org
+       "/boards/"
+       section))
+
 (defn storage-request-post-url
   [org section uuid]
-  (str config/storage-server-url "/orgs/" org "/entries/" uuid))
+  (str config/storage-server-url
+       "/orgs/"
+       org
+       "/boards/"
+       section
+       "/entries/"
+       uuid))
 
 (defn storage-request-secure-uuid-url
   [org section uuid]
   (str config/storage-server-url "/orgs/" org "/entries/" uuid))
 
-(defn get-post-data
-  [org section uuid token cb]
-  (http/get (storage-request-post-url org section uuid) (get-post-options token)
+(defn get-data
+  [request-url token cb]
+  (http/get request-url (get-post-options token)
     (fn [{:keys [status headers body error]}]
       (if error
         (timbre/error "Failed, exception is " error)
         (let [parsed-body (json/parse-string body)]
           (cb parsed-body))))))
 
-(defn update-slack-url
-  "Posts back to the Slack API to add information to one of our urls.
-   https://api.slack.com/docs/message-link-unfurling
-   Also see open-company-lib
-  " 
-  [token channel ts url post-data]
+(defn org-unfurl-data
+  [url org-data]
+  (let [org-name (get org-data "name")
+        org-logo (get org-data "logo-url")
+        title (str org-name " on Carrot")
+        content "Carrot keeps everyone aligned around what matters most."
+        section-count (count (get org-data "boards"))
+        footer (str org-name
+                    " | "
+                    section-count
+                    (if (= 1 section-count)
+                      " section "
+                      " sections "))
+        url-text (get url "url")]
+  (json/encode {url-text
+                {
+                 :author_name org-name
+                 :author_icon org-logo
+                 :title title
+                 :title_link url-text
+                 :text content
+                 :footer footer
+                 :attachment_type "default"
+                 :color "good" ;; this can be a hex color
+                 }})))
+
+(defn section-unfurl-data
+  [url section-data]
+  (let [org-data (:org-data section-data)
+        org-name (get org-data "name")
+        org-logo (get org-data "logo-url")
+        title (get section-data "name")
+        content (str "A section of the " org-name " digest.")
+        post-count (count (get section-data "entries"))
+        footer (str title
+                    " | "
+                    post-count
+                    (if (= 1 post-count)
+                      " post "
+                      " posts "))
+        url-text (get url "url")]
+  (json/encode {url-text
+                {
+                 :author_name org-name
+                 :author_icon org-logo
+                 :title title
+                 :title_link url-text
+                 :text content
+                 :footer footer
+                 :attachment_type "default"
+                 :color "good" ;; this can be a hex color
+                 }})))
+
+(defn post-unfurl-data
+  [url post-data]
   (let [content (.text (soup/parse (get post-data "body")))
         title (.text (soup/parse (get post-data "headline")))
         board-slug (get post-data "board-slug")
@@ -77,39 +148,69 @@
                     )
         org (get post-data "org-name")
         org-logo (get post-data "org-logo-url")
-        url-text (get url "url")
-        url-data (json/encode {url-text
-                               {
-                                :author_name org
-                                :author_icon org-logo
-                                :title title
-                                :title_link url-text
-                                :text content
-                                :footer footer
-                                :attachment_type "default"
-                                :color "good" ;; this can be a hex color
-                                }})]
-    (timbre/debug url-data)
-    (timbre/debug
-     (slack-lib/unfurl-post-url token channel ts url-data)
-     )
-    )
-  )
+        url-text (get url "url")]
+    (json/encode {url-text
+                  {
+                   :author_name org
+                   :author_icon org-logo
+                   :title title
+                   :title_link url-text
+                   :text content
+                   :footer footer
+                   :attachment_type "default"
+                   :color "good" ;; this can be a hex color
+                   }})))
+
+(defn update-slack-url
+  "Posts back to the Slack API to add information to one of our urls.
+   https://api.slack.com/docs/message-link-unfurling
+   Also see open-company-lib for slack unfurl function.
+  "
+  [token channel ts url data]
+  (timbre/debug "RETUNED DATA: " data)
+  (when data
+    (let [url-data (cond
+
+                    (and (get data "headline") (get data "body"))
+                    (post-unfurl-data url data)
+
+                    (and (get data "slug") (get data "entries"))
+                    (section-unfurl-data url data)
+
+                    (and (get data "slug") (get data "boards"))
+                    (org-unfurl-data url data)
+
+                    :default
+                    false
+                    )]
+      (timbre/info
+       (when url-data (slack-lib/unfurl-post-url token channel ts url-data))))))
 
 (defn parse-carrot-url [url]
   (let [split-url (clojure.string/split (get url "url") #"/")
         org (nth split-url 3)
         split-count (count split-url)]
-    (timbre/debug split-url org split-count)
+
     (cond
-     
+
+     ;;http://carrot.io/carrot/general
+     (= 4 split-count)
+     {:org org
+      :url-type "org"}
+
+     ;;http://carrot.io/carrot/general
+     (= 5 split-count)
+     {:org org
+      :section (nth split-url 4)
+      :url-type "section"}
+
      ;;http://carrot.io/carrot/general/post/78ba-40f0-bbb5
      (= 7 split-count)
      {:org org
       :section (nth split-url 4)
       :url-type "post"
       :uuid (nth split-url 6)}
-     
+
      ;;http://carrot.io/carrot/post/78ba-40f0-bbb5
      (= 6 split-count)
      {:org org
@@ -124,18 +225,56 @@
   ;; split url
   (let [parsed-link (parse-carrot-url link)
         decoded-token (jwt/decode token)
-        slack-token (:slack-token (:claims decoded-token))]
-    ;; is this a post?
-    (when (= "secure-uuid" (:url-type parsed-link))
-      ;; ask the storage service for information
-      (get-post-data (:org parsed-link)
-                     (:section parsed-link)
-                     (:uuid parsed-link)
-                     token
-                     (fn [data]
-                       (update-slack-url
-                        slack-token
-                        channel
-                        message_ts
-                        link
-                        data))))))
+        slack-token (:slack-token (:claims decoded-token))
+        url-type (:url-type parsed-link)
+        request-url (cond
+                     (= "secure-uuid" url-type)
+
+                     (storage-request-secure-uuid-url
+                      (:org parsed-link)
+                      (:section parsed-link)
+                      (:uuid parsed-link))
+
+                     (= "post" url-type)
+
+                     (storage-request-post-url
+                      (:org parsed-link)
+                      (:section parsed-link)
+                      (:uuid parsed-link))
+
+                     (= "section" url-type)
+
+                     (storage-request-section-url
+                      (:org parsed-link)
+                      (:section parsed-link))
+
+                     (= "org" url-type)
+                     (storage-request-org-url
+                      (:org parsed-link))
+
+                     :default
+                     nil)]
+
+    (when request-url
+      (if (= "section" url-type)
+        (get-data (storage-request-org-url (:org parsed-link))
+                  token
+                  (fn [org-data]
+                    (get-data request-url
+                              token
+                              (fn [data]
+                                (update-slack-url
+                                 slack-token
+                                 channel
+                                 message_ts
+                                 link
+                                 (assoc data :org-data org-data))))))
+        (get-data request-url
+                  token
+                  (fn [data]
+                    (update-slack-url
+                     slack-token
+                     channel
+                     message_ts
+                     link
+                     data)))))))
