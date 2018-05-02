9b3468366809f338bc4c943dfe9cf8acbb852a0c
Nathan Zorn <nathan.zorn@gmail.com>
2018-05-02 16:52:28 -0400

    config cleanup

--- src/oc/slack_router/config.clj
+++ src/oc/slack_router/config.clj
@@ -26,11 +26,10 @@
 ;; ----- HTTP server -----
 
 (defonce hot-reload (bool (or (env :hot-reload) false)))
-(defonce auth-server-port (Integer/parseInt (or (env :port) "3003")))
+(defonce auth-server-port (Integer/parseInt (or (env :auth-server-port) "3003")))
 (defonce slack-router-server-port (Integer/parseInt (or (env :port) "3009")))
-(defonce storage-server-port (Integer/parseInt (or (env :port) "3001")))
+(defonce storage-server-port (Integer/parseInt (or (env :storage-server-port) "3001")))
 ;; ----- Liberator -----
-
 ;; see header response, or http://localhost:3009/x-liberator/requests/ for trace results
 (defonce liberator-trace (bool (or (env :liberator-trace) false)))
 (defonce pretty? (not prod?)) ; JSON response as pretty?
@@ -41,8 +40,7 @@
 (defonce slack-router-server-url (or (env :slack-router-server-url) (str "http://localhost:" slack-router-server-port)))
 (defonce ui-server-url (or (env :ui-server-url) "http://localhost:3559"))
 
-;; ----- AWS SQS -----
-
+;; ----- AWS -----
 (defonce aws-access-key-id (env :aws-access-key-id))
 (defonce aws-secret-access-key (env :aws-secret-access-key))
 
