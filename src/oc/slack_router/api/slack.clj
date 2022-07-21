354177275b3f7e473b7f53e081365d6c8c710bd1
bago2k4 <iacopo@rc0.eu>
2022-07-21 07:03:47 +0200

    Add more debug statements.

--- src/oc/slack_router/api/slack.clj
+++ src/oc/slack_router/api/slack.clj
@@ -20,6 +20,7 @@
         team-id (:team_id body)
         message_ts (:message_ts event)
         channel (:channel event)]
+    (timbre/trace "Slack unfurl request with token %s and body %s" token body)
     (doseq [link links]
       ;; Post back to slack with added info
       (slack-unfurl/unfurl token team-id channel link message_ts))
@@ -159,6 +160,7 @@
                           config/auth-server-url
                           config/passphrase
                           "Slack Router")
+             _ (timbre/trace "Retrieved token %s" user-token)
              unfurl-outcome (render-slack-unfurl user-token body)]
       [true unfurl-outcome]
       (let [emessage (format "Missing jwt token for user: %s" slack-user)]
